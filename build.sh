a5d94ae65fd1f3e1dd16eab893975f47242ce40e
Ruediger Gad <rgad@fb2.fh-frankfurt.de>
2012-05-22 12:18:21 +0200

    Remove .git directories on build as well.

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 
 find . -name *.qml -exec sed -i 's/Qt 4\.7/QtQuick 1\.1/g' {} \;
 mv qtc_packaging/debian_harmattan debian
-find . -name .svn -exec rm -rf {} \;
+find . -name ".svn" -exec rm -rf {} \;
+find . -name ".git" -exec rm -rf {} \;
 rm -rf qtc_packaging
 fakeroot dpkg-buildpackage -sa
 
